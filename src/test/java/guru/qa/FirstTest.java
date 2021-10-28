61b60fb6bf5def4d3552d1a239ae19ff90ce7b6d
viktoria.klisheva <vikmah2@gmail.com>
2021-10-29 02:07:20 +0300

    Черновик2

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -6,20 +6,31 @@ import org.openqa.selenium.By;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class FirstTest {
     @Test
     void firstTest() {
         open ("https://demoqa.com/automation-practice-form");
-        $("[id=firstName]").setValue("Vitoriia");
-        $("[id=lastName]").setValue("Sun");
-        $("#userEmail").setValue("IronLady@gmail.com");
-        $("[for=gender-radio-1]").click();
-        $("[id=userNumber]").setValue("9302764785");
-        $("[for=dateOfBirthInput]").click(); //списала с чатика + календарь не готов и дальше тест падает
-        $("#subjectsContainer").setValue("test");
+        $("[id=firstName]").setValue("Vitoriia");     //заполняеи поле имя
+        $("[id=lastName]").setValue("Sun");     //заполняеи поле фамилия
+        $("#userEmail").setValue("IronLady@gmail.com"); // заполняем эл.почту
+        $("[for=gender-radio-1]").click();    //выбираем пол
+        $("[id=userNumber]").setValue("9302764785");   //заполняет тел
+        $("#dateOfBirthInput").click();   //открываем календарь
+        $(".react-datepicker__month-select").selectOptionByValue("4");  //выбираем месяц
+        $(".react-datepicker__year-select").selectOptionByValue("1993");  //выбираем год
+        $(".react-datepicker__day").click();   //выбираем рандомную дату
+        $("#subjectsInput").click();  //кликаем на поле с выпадающим списком
+        $("#subjectsInput").setValue("d");  //вбиваем букву ,чтобы появился выпадающий список
+        $(byText("Hindi")).click();   //выбираем значение из списка
+        $("[for=hobbies-checkbox-1]").click();    //заполняет увлечения
+
+
+        $("[for=gender-radio-1]").setValue("test");
         $("#hobbies-checkbox-3").click();
         $("[#upload]").uploadFile(new File("cv.doc")); //списала с чатика
         $("[]").setValue("");
